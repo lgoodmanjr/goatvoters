bd9858773d9477bc5bb25aa5c707ac771ddc66dc
lgoodmanjr <lgoodmanjr@gmail.com>
2026-05-27 16:13:42 -0400

    Create Divider.jsx

---
+++ src/components/Divider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Divider() {
+  return (
+    <div style={{
+      height: '1px',
+      background: 'var(--border)',
+      margin: '0 1.25rem 1.25rem',
+    }} />
+  )
+}
